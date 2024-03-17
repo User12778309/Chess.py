91a87baadbc5011bc9b5b3d61bbf615a6edaac6a
User12778309 <sammy12778309@gmail.com>
2024-03-17 16:46:59 +0100

    add some file ( error )

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from python_class.game import Game
 import pygame
 import sys
 
@@ -10,8 +11,10 @@ pygame.init()
 screen = pygame.display.set_mode((ligne * cellule,colonne * cellule))
 pygame.display.set_caption("Chess")
 
-background_image = pygame.image.load("asets/chess board.png")
+background_image = pygame.image.load("assets/chess board.png")
 background = pygame.transform.scale(background_image,(900,900))
+
+game = Game()
 
 while True:
 
@@ -21,5 +24,7 @@ while True:
             sys.exit()
 
     screen.blit(background,(-50,-50))
+
+    game.draw()
 
     pygame.display.flip()
